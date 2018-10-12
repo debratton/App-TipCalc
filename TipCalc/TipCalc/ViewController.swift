0123a9346279a14f69ee6412593cbef39de7bdd4
debratton <debratton@gmail.com>
2018-10-12 09:37:24 -0500

    Start Project

---
+++ TipCalc/TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by David E Bratton on 10/12/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
